af1472d2666175ad76dee2b735aa222c80a79eee
202504_nguyentn <tomato.202504.nguyentn@gmail.com>
2026-03-24 12:59:39 +0900

    add: base url

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,9 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite";
 
 // Vite設定ファイル
 export default defineConfig({
-  base: '/service-now-csa/',
-  plugins: [
-    react(),
-    tailwindcss(),
-  ],
-})
+  base: "/service-now-csa/",
+  plugins: [react(), tailwindcss()],
+});
